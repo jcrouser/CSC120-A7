660f10b57c68947daa4d43fde24e19f72af59bf5
R. Jordan Crouser <jcrouser@smith.edu>
2022-10-26 13:42:54 -0500

    Add files via upload

---
+++ Building.java
@@ -0,0 +1,33 @@
+public class Building {
+
+    private String name = "<Name Unknown>";
+    private String address = "<Address Unknown>";
+    private int nFloors = 1;
+
+    public Building(String name, String address, int nFloors) {
+        if (name != null) { this.name = name; }
+        if (address != null) { this.address = address; } 
+        if (nFloors < 1) {
+            throw new RuntimeException("Cannot construct a building with fewer than 1 floor.");
+        }
+        this.nFloors = nFloors;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public int getFloors() {
+        return this.nFloors;
+    }
+
+    public String toString() {
+        return this.name + " is a " + this.nFloors + "-story building located at " + this.address;
+    }
+
+    public static void main(String[] args) {
+        Building fordHall = new Building("Ford Hall", "100 Green Street Northampton, MA 01063", 4);
+        System.out.println(fordHall);
+    }
+
+}
