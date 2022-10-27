c97abd154aba0a34d4104565f20679ca245cab33
R. Jordan Crouser <jcrouser@smith.edu>
2022-10-26 20:35:29 -0500

    Add files via upload

---
+++ Cafe.java
@@ -0,0 +1,12 @@
+/* This is a stub for the Cafe class */
+public class Cafe {
+
+    public Cafe() {
+        System.out.println("You have built a cafe: ☕");
+    }
+    
+    public static void main(String[] args) {
+        new Cafe();
+    }
+    
+}
